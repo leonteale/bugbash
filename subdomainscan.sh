4710550e2c404fa71fd8278e8f4d4a8e42dc552a
Leon Teale <leonteale89@gmail.com>
2023-02-18 14:50:38 +0000

    Update subdomainscan.sh

--- subdomainscan.sh
+++ subdomainscan.sh
@@ -7,7 +7,7 @@ fi
 # check if subfinder is installed and run it
 if which subfinder >/dev/null 2>&1; then
   echo "Running subfinder against $Domain"
-  subfinder -d $Domain -all -v -o "${WD}/subdomains.${Domain}.txt"
+  subfinder -d $Domain -v -o "${WD}/subdomains.${Domain}.txt"
 else
   echo "subfinder not found, please install it manually."
 fi
