74e598e0dd93c6a060884475d037bf8fbf135a31
Leon Teale <leonteale89@gmail.com>
2023-02-18 14:50:02 +0000

    Update subdomainscan.sh

--- subdomainscan.sh
+++ subdomainscan.sh
@@ -1,7 +1,7 @@
 # check if subfinder is installed
 if ! which subfinder >/dev/null 2>&1; then
   echo "subfinder not found, installing..."
-  sudo apt-get -y install subfinder
+  go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 fi
 
 # check if subfinder is installed and run it
